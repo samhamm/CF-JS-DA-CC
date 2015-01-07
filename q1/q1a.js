5359ff48fa47cb6d5ec295e0b7464ac61aa87653
samhamm <samhamm@gmail.com>
2015-01-07 13:33:51 -0800

    adds mvp of 1a

---
+++ q1/q1a.js
@@ -0,0 +1,48 @@
+// var _ = require("http://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.js");
+
+var people = [
+  {
+    name: 'Bob',
+    occupation: 'programmer',
+    awesome_index: 34,
+  },
+  {
+    name: 'Alice',
+    occupation: 'programmer',
+    awesome_index: 26,
+  },
+  {
+    name: 'Zaphod',
+    occupation: 'President of the Galaxy',
+    awesome_index: 10
+  }
+];
+
+var average = 0,
+    progs = 0,
+    total = 0;
+
+function display() {
+  var peopleList = [];
+  _.forEach(people, function(people) {
+    peopleList.push("<br>" + people.name + " is a " + people.occupation + " with an Awesome Index of " + people.awesome_index);
+  });
+  $("#ppl-list").html(peopleList);
+  $("#awesome-index").html(average);
+}
+
+function awesomeAvg(people) {
+  _.forEach(people, function(people){
+    if (people.occupation === "programmer") {
+      progs++;
+      total += people.awesome_index;
+      average = total / progs;
+    }
+  });
+}
+
+awesomeAvg(people);
+display();
+
+// constructor to add new people
+// call to recalculate average when a new person is added
