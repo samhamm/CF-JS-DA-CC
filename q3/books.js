ffcde8d28a98e9b5b8abe7151263bb774b0dfcd6
samhamm <samhamm@gmail.com>
2015-01-07 23:52:28 -0800

    framework of q3; still needs nearly all wiring

---
+++ q3/books.js
@@ -0,0 +1,86 @@
+var books = [
+  {
+    author: 'Quark, Mel',
+    title: 'JavaScratch: Programming for Kittens',
+    category: 'dev',
+    callNum: 'Q.1',
+    status: 'in'
+  },
+  {
+    author: 'Ewing, Cris',
+    title: 'The Insatiable Python',
+    category: 'dev',
+    callNum: 'E.1',
+    status: 'in'
+  },
+  {
+    author: 'Riggio, Brook',
+    title: 'Maritime Navigation: Rails for Sails',
+    category: 'dev',
+    callNum: 'R.1',
+    status: 'in'
+  },
+  {
+    author: 'Sande, Dale',
+    title: 'You Can All Kiss My Sass',
+    category: 'dev',
+    callNum: 'S.1',
+    status: 'in'
+  },
+  {
+    author: 'D-Jung',
+    title: 'Trucking It Thai-Style',
+    category: 'menus',
+    callNum: 'D.1',
+    status: 'in'
+  },
+  {
+    author: 'Top Pot',
+    title: 'Donuts Are Like Palindromes',
+    category: 'menus',
+    callNum: 'T.1',
+    status: 'in'
+  },
+  {
+    author: 'Berliner Döner',
+    title: 'Kebabs for Flavor and Profit',
+    category: 'menus',
+    callNum: 'B.1',
+    status: 'in'
+  },
+  {
+    author: 'Starbucks',
+    title: 'Burnt Coffee',
+    category: 'menus',
+    callNum: 'S.2',
+    status: 'in'
+  },
+  {
+    author: 'Storck, Ivan',
+    title: 'Thick and Full',
+    category: 'beards',
+    callNum: 'S.3',
+    status: 'in'
+  },
+  {
+    author: 'Ewing, Cris',
+    title: 'Trim and Tight',
+    category: 'beards',
+    callNum: 'E.2',
+    status: 'in'
+  },
+  {
+    author: 'Morgan, Tyler',
+    title: 'The Emo Lumberjack',
+    category: 'beards',
+    callNum: 'M.1',
+    status: 'in'
+  },
+  {
+    author: 'Hamm, Sam',
+    title: 'Escargoatee: Beard-Trapped Gourmet',
+    category: 'beards',
+    callNum: 'H.1',
+    status: 'in'
+  },
+];
---
+++ q3/shelves.js
@@ -0,0 +1,6 @@
+function addBook() {}
+
+function removeBook() {}
+
+addBook();
+removeBook();
